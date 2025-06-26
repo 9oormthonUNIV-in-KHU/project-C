40873109fb51670e6e382673eed093e7d0f47ead
Son Hyunbin <147061193+lemonson03@users.noreply.github.com>
2025-06-26 18:29:05 +0900

    Update JwtAuthenticationFilter.java

--- src/main/java/com/example/cardcase/common/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/cardcase/common/security/JwtAuthenticationFilter.java
@@ -20,8 +20,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
 
     private static final List<String> NO_AUTH_PATHS = List.of(
-            "/api/auth/signup",
-            "/api/auth/login"
+            "/api/v1/auth/signup",
+            "/api/v1/auth/login"
     );
 
     @Override
