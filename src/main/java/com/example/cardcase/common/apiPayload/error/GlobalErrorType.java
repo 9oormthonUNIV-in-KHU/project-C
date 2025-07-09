f4d1988d66f3e823bafea525292e7c50c1ca55bf
junstory <lastcom1@gmail.com>
2025-07-09 20:27:07 +0900

    Update: Error status 수정하기
    Signed-off-by: junstory <lastcom1@gmail.com>

--- src/main/java/com/example/cardcase/common/apiPayload/error/GlobalErrorType.java
+++ src/main/java/com/example/cardcase/common/apiPayload/error/GlobalErrorType.java
@@ -10,7 +10,7 @@ public enum GlobalErrorType implements ErrorType {
 
     E500(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 내부 오류입니다."),
     MEMBER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "Member Not Found."), // "존재하지 않는 사용자" 401 Unauthorized
-    NOT_ALLOWED(HttpStatus.UNAUTHORIZED, "권한이 없습니다."),
+    NOT_ALLOWED(HttpStatus.FORBIDDEN, "권한이 없습니다."),
     CARD_NOT_FOUND(HttpStatus.NOT_FOUND, "카드를 찾을 수 없습니다."),
     ;
 
